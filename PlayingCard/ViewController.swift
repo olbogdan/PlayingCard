f75cbfabb2158ddc5646ecf2ba3679015719cdbb
bogdanov <iceposhta@gmail.com>
2021-03-28 17:36:14 +0100

    flip card on long press

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -8,32 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     var deck = PlayingCardDeck()
-    
-    @IBOutlet weak var playingCardView: PlayingCardView! {
+
+    @IBOutlet var playingCardView: PlayingCardView! {
         didSet {
             let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
             swipe.direction = [.left, .right]
             playingCardView.addGestureRecognizer(swipe)
         }
     }
-    
+
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default:
+            break
+        }
+    }
+
     @objc func nextCard() {
         if let card = deck.draw() {
             playingCardView.rank = card.rank.order
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        for _ in 1...10 {
+        for _ in 1 ... 10 {
             if let card = deck.draw() {
                 print("\(card)")
             }
         }
     }
-
-
 }
-
