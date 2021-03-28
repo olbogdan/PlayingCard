e0f2e60757124ac52b87f8ffc87e24087710d5f1
bogdanov <iceposhta@gmail.com>
2021-03-28 17:20:19 +0100

    show next card on swipe

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -11,6 +11,20 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
     
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         for _ in 1...10 {
